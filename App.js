2145794da9e8fd30ca8ff4b16417e7c470d0cc1d
IvanSeagull <ivan.seagull.k@gmail.com>
2021-07-11 21:47:34 +0100

    made frontend of the app

--- App.js
+++ App.js
@@ -2,11 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Task from './components/Task';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.wrapper}>
+        <Text style={styles.section_title}>Today's tasks</Text>
+
+        <View style={styles.items}>
+          {/* Tasks */}
+          <Task text={'Task 1'}/>
+          <Task text={'Task 2'}/>
+
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +25,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8eaed',
   },
+  wrapper:{
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  section_title:{
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items:{
+    marginTop: 30
+  }
+
 });
